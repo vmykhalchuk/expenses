5e677f467ebf7ac0034bd772534f48b29d3d1427
Volodymyr Mykhalchuk <vmykhalchuk@vertafore.com>
2021-06-18 00:36:42 +0300

    Updated gasToGit.bat to first show git diff and only then let user to enter commit message

--- gasToGit.bat
+++ gasToGit.bat
@@ -1,14 +1,18 @@
 @echo off
 setlocal
-set /p gitmessage="Enter commit message: "
-rem trim gitmessage from the left (no good to code to trim right)
-for /f "tokens=* delims= " %%a in ("%gitmessage%") do set gitmessage=%%a
-if "!%gitmessage%~" == "!~" goto END
 
 call git pull
 cd gas
 call clasp pull
 cd ..
+
+git diff
+
+set /p gitmessage="Enter commit message: "
+rem trim gitmessage from the left (no good to code to trim right)
+for /f "tokens=* delims= " %%a in ("%gitmessage%") do set gitmessage=%%a
+if "!%gitmessage%~" == "!~" goto END
+
 git add .
 git commit -m "%gitmessage%" -m "Gas to Git commit %date% %time%"
 call git push
