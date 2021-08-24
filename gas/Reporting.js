14dbf8b66bed31537b0da0f25513c6e4acb62100
Volodymyr Mykhalchuk <vmykhalchuk@vertafore.com>
2021-08-24 23:07:30 +0300

    [CODE] added weekly reporting sheet code

--- gas/Reporting.js
+++ gas/Reporting.js
@@ -47,8 +47,9 @@ var _reporting = {
       var diSheet = this.reportingSs.getActiveSheet();
       diSheet.setName("Data Ingest");
       var mainSsId = _sheets.getMainSsId();
-      var ingestFormula = "=QUERY({IMPORTRANGE(\"" + mainSsId + "\", \"AidTx!A3:Q\");IMPORTRANGE(\"" + mainSsId + "\", \"ArchTx!A3:Q\");IMPORTRANGE(\"" + mainSsId + "\", \"InTx!A3:Q\")}," +
-        "\"SELECT Col3, Col4, Col5, Col7, Col10, Col11, Col16, Col17 WHERE Col3 > date '1982-11-18' AND Col11<>'_none' AND Col11<>'_other' AND Col11<>'_split_2' AND Col11<>'_split_3' AND Col11<>'_split_4' AND Col11<>''\")";
+      var aidValuesRange = "AidTx!A3:Q";
+      var ingestFormula = '=QUERY({IMPORTRANGE("' + mainSsId + '", "' + aidValuesRange + '");IMPORTRANGE(\"" + mainSsId + "\", \"ArchTx!A3:Q\");IMPORTRANGE(\"" + mainSsId + "\", \"InTx!A3:Q\")},' +
+        "\"SELECT Col3, Col4, Col5, Col7, Col10, Col11, Col16, Col17 WHERE Col3 > date '2011-11-17' AND Col11<>'_none' AND Col11<>'_other' AND Col11<>'_split_2' AND Col11<>'_split_3' AND Col11<>'_split_4' AND Col11<>''\")";
       var headerColNames = [["TX Date", "Amount UAH", "Currency", "Mono MCC", "Tx Type", "Exp Type", "Misc\nSub Type", "House\nSub Type"]];
       
       var firstRowRange = diSheet.getRange(1, 1, 1, headerColNames[0].length);
@@ -80,7 +81,6 @@ var _reporting = {
           "\"Select Col3,Col6,Col7,Col8,Sum(Col2) where Col1>0 group by Col6,Col7,Col8,Col3 Pivot Col1 Label Col3'Descr / Month'\",0)";
       mrSheet.getRange("B4").setFormula(formula);
       mrSheet.getRange("B4").setBackground("lightgray");
-      mrSheet.getRange("B4").setFontWeight("bold");
 
       mrSheet.getRange("A5:ZZ50").setNumberFormat("#,##0.00");
       
@@ -98,6 +98,38 @@ var _reporting = {
     
     createWeeklyReportSheet: function() {
       var wrSheet = this.reportingSs.insertSheet("Report Weekly");
+
+      wrSheet.getRange("A1").setValue("US Week Numbers here:");
+      wrSheet.getRange("B1").setValue("https://www.timeanddate.com/calendar/?year=2021&country=1&wno=1");
+
+      wrSheet.getRange("E2").setValue("YEAR:");
+      wrSheet.getRange("E3").setValue("WEEK:");
+      wrSheet.getRange("F2").setFormula('=ARRAYFORMULA(IF(ISBLANK(F4:ZZ4),"",FLOOR(F4:ZZ4 / 100)))');
+      wrSheet.getRange("F3").setFormula('=ARRAYFORMULA(IF(ISBLANK(F4:ZZ4),"",MOD(F4:ZZ4,100)))');
+      wrSheet.getRange("E2:F3").setBackground("lightgray");
+      wrSheet.getRange("A2:ZZ3").setFontWeight("bold");
+
+      var isName = "Data Ingest";
+      var txDateRage = "'" + isName + "'!A2:A";
+      var otherDataRange = "'" + isName + "'!B2:H";
+      var formula = "=query({ArrayFormula(if(len(" + txDateRage + "),(YEAR(" + txDateRage + ")*100+WEEKNUM(" + txDateRage + ")),))," +
+        "query(" + otherDataRange + ")}," +
+          "\"Select Col3,Col6,Col7,Col8,Sum(Col2) where Col1>0 group by Col6,Col7,Col8,Col3 Pivot Col1 Label Col3'Descr / Week №'\",0)";
+      wrSheet.getRange("B4").setFormula(formula);
+      wrSheet.getRange("B4").setBackground("lightgray");
+
+      wrSheet.getRange("A5:ZZ50").setNumberFormat("#,##0.00");
+
+      for (var i = 5; i <= 50; i++) {
+        wrSheet.getRange("A" + i).setFormula("=SUM(F" + i + ":" + i + ")")
+      }
+      wrSheet.getRange("A5:A50").setBackground("lightgray");
+      wrSheet.getRange("A5:A50").setFontWeight("bold");
+
+      wrSheet.hideRows(4);
+      wrSheet.setFrozenColumns(5);
+      wrSheet.setFrozenRows(4);
+      wrSheet.protect();
     },
     
     deleteSpreadsheet: function() {
